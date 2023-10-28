e37bf7d5b6d935299a0a50181bad018ab30f3398
Marcela Starling <marcela.sfl@hotmail.com>
2023-10-28 20:00:18 -0300

    Crud de tipo de epicos

--- src/main/java/com/example/geradorus/repository/TipoEpicoRepository.java
+++ src/main/java/com/example/geradorus/repository/TipoEpicoRepository.java
@@ -1,8 +1,9 @@
 package com.example.geradorus.repository;
 
 import com.example.geradorus.model.Epico;
+import com.example.geradorus.model.TipoEpico;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EpicoRepository extends JpaRepository<Epico, Long> {
+public interface TipoEpicoRepository extends JpaRepository<TipoEpico, Long> {
 
 }
