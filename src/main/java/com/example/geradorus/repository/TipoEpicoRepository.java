f7d5cca59b60a7626e8b3fc9526a698f94fc6880
Isabella Sampaio <71940799+IsabellaSampaio@users.noreply.github.com>
2023-10-28 22:43:10 -0300

    Delete src directory

--- src/main/java/com/example/geradorus/repository/TipoEpicoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.geradorus.repository;
-
-import com.example.geradorus.model.Epico;
-import com.example.geradorus.model.TipoEpico;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TipoEpicoRepository extends JpaRepository<TipoEpico, Long> {
-
-}
